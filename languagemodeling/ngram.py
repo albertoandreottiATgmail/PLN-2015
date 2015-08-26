10cbccc6d2973d8e2f259efc62470d94b2f98369
albertoandreotti <albertoa@vsisoft.com>
2015-08-26 13:09:11 -0300

    excercises 1 & 2: first version with test cases.

--- languagemodeling/ngram.py
+++ languagemodeling/ngram.py
@@ -1,6 +1,6 @@
 # https://docs.python.org/3/library/collections.html
 from collections import defaultdict
-
+from math import pow, log
 
 class NGram(object):
 
@@ -27,3 +27,60 @@ class NGram(object):
 
         tokens = prev_tokens + [token]
         return float(self.counts[tuple(tokens)]) / self.counts[tuple(prev_tokens)]
+
+
+    def count(self, tokens):
+        """Count for an n-gram or (n-1)-gram.
+ 
+        tokens -- the n-gram or (n-1)-gram tuple.
+        """
+        return self.counts[tuple(tokens)]
+
+    def cond_prob(self, token, prev_tokens=None):
+        """Conditional probability of a token.
+ 
+        token -- the token.
+        prev_tokens -- the previous n-1 tokens (optional only if n = 1).
+        """
+        if self.n is not 1 and prev_tokens is None:
+            return 0.0 # TODO: throw!
+
+        counts = self.counts
+        if prev_tokens == None:
+            total = lambda z: True
+            num = counts[tuple(token)]
+        else:
+            total = lambda z: z[:-1] == tuple(prev_tokens)
+            local_prev_tokens = list(prev_tokens)
+            local_prev_tokens.append(token[0])
+            num = counts[tuple(local_prev_tokens)]
+        
+        
+        keys = filter(total, counts)
+        print keys
+        denom = reduce(lambda x, y: x + y, map(lambda key: counts[key], keys))
+        return float(num) / denom
+ 
+    def sent_prob(self, sent):
+        """Probability of a sentence. Warning: subject to underflow problems.
+ 
+        sent -- the sentence as a list of tokens.
+        """
+        pow(2, self.sent_log_prob(sent))
+
+ 
+    def sent_log_prob(self, sent):
+        """Log-probability of a sentence.
+ 
+        sent -- the sentence as a list of tokens.
+        """
+        probabilities = [self.cond_prob(sent[i : i + self.n - 1], sent[i + self.n]) for i in range(len(sent) - 1)]
+        #append beginning and end
+        #probabilities.append()
+        #probabilities.append()
+        probabilities = [log(prob, 2) for prob in probabilities]
+        reduce(lambda x, y:  x + y, probabilities)
+
+
+
+
---
+++ languagemodeling/scripts/leipzigreader.py
@@ -0,0 +1,34 @@
+## A reader for files in the Leipzig corpus ##
+## file : http://corpora2.informatik.uni-leipzig.de/downloads/eng-za_web_2013_100K-text.tar.gz ##
+import re
+import itertools
+from nltk.corpus import PlaintextCorpusReader
+import os 
+
+class LeipzigCorpusReader(PlaintextCorpusReader):
+    
+    def __init__(self, fname):
+        with open(os.path.dirname(os.path.realpath(__file__)) + '/' + fname) as f:
+            content = f.readlines()
+
+        self._sentences = [re.sub(r'^.+\\t', '', sentence) for sentence in content]
+          
+
+    def words(self, fileids=None): 
+        """ 
+        :return: the given file(s) as a list of words and punctuation symbols. 
+        :rtype: list(str) 
+        """
+
+        def flatten(listOfLists):
+            "Flatten one level of nesting"
+            return itertools.chain.from_iterable(listOfLists)
+
+        return flatten([sentence.split() for sentence in self._sentences]) 
+
+    def sents(self, fileids=None): 
+        """ 
+        :return: the given file(s) as a list of sentences or utterances, each encoded as a list of word strings. 
+        :rtype: list(list(str)) """ 
+    
+        return [sentence.split() for sentence in self._sentences]
