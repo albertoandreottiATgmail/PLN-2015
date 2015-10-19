9c7dac9b08ffe983e1ce50bdaba915b1336a677e
albertoandreotti <albertoandreotti@gmail.com>
2015-10-19 15:58:41 -0300

    Exercise 6: basic features.

--- tagging/features.py
+++ tagging/features.py
@@ -1,3 +1,4 @@
+
 from collections import namedtuple
 
 from featureforge.feature import Feature
@@ -11,8 +12,65 @@ History = namedtuple('History', 'sent prev_tags i')
 
 def word_lower(h):
     """Feature: current lowercased word.
-
+    
     h -- a history.
     """
     sent, i = h.sent, h.i
     return sent[i].lower()
+
+def word_isupper(h):
+    """Feature: is current word uppercase?
+    
+    h -- a history.
+    """
+    sent, i = h.sent, h.i
+    return sent[i].isupper()    
+
+def word_istitle(h):
+    """Feature: is current word a title?
+    
+    h -- a history.
+    """   
+    sent, i = h.sent, h.i
+    return sent[i][0].isupper()   
+
+def word_isdigit(h):
+    """Feature: current lowercased word.
+    
+    h -- a history.
+    """   
+    sent, i = h.sent, h.i
+    return sent[i].l()
+
+
+
+
+class NPrevTags(Feature):
+ 
+    def __init__(self, n):
+        """Feature: n previous tags tuple.
+ 
+        n -- number of previous tags to consider.
+        """
+ 
+    def _evaluate(self, h):
+        """n previous tags tuple.
+ 
+        h -- a history.
+        """
+ 
+ 
+class PrevWord(Feature):
+ 
+    def __init__(self, f):
+        """Feature: the feature f applied to the previous word.
+ 
+        f -- the feature.
+        """
+ 
+    def _evaluate(self, h):
+        """Apply the feature to the previous word in the history.
+ 
+        h -- the history.
+        """
+
